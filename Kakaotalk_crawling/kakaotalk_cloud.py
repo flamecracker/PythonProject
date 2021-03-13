e4b960f843999d04226784fdb0c3453158f5a7f7
flamecracker <flamecracker1220@gmail.com>
2021-03-13 18:14:50 +0900

    210313수정

--- Kakaotalk_crawling/kakaotalk_cloud.py
+++ Kakaotalk_crawling/kakaotalk_cloud.py
@@ -1,10 +1,16 @@
-import os, sys
+import os
 from wordcloud import WordCloud
 
-def kakaotalk_printer(txt_file,result_txt):
-    with open(txt_file,"r",encoding="utf-8") as fr, open(result_txt,"w",encoding="utf-8") as fw:
+def found_kakao_txt():
+    txt_path = os.getcwd()+"\\Kakaotalk_crawling\\txt"
+    txt_list = os.listdir(txt_path)
+    return txt_list
+
+def kakao_conv_rate(txt_file):
+    with open(txt_file,"r",encoding="utf-8") as fr:
         lines = fr.readlines()
         error_words=["이모티콘","ㅋㅋ","사진"]
+        text = ""
         for line in lines[5:]: # 위의 5줄은 필요없으니 날린다.
             if '] [' in line:
                 index = -1
@@ -18,35 +24,53 @@ def kakaotalk_printer(txt_file,result_txt):
                 line = line[start+2:]
                 for error_word in error_words:
                     line = line.replace(error_word,"")                 
-                fw.write(line)
+                text += line
+    return text
 
-    fr.close()
-    fw.close()
-
-def found_kakao_txt():
-    os.chdir(os.getcwd()+"\\Kakaotalk_crawling")
-    txt_path = os.getcwd()+"\\txt"
-    # print(txt_path)
-    txt_list = os.listdir(txt_path)
-    return txt_list
-
-def kakao_to_cloud(text_name, i, wordcloud):
-    text = ''
-    with open(text_name, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-        for line in lines:
-            text += line
-    wordcloud.generate(text)
-    wordcloud.to_file(result_png)
-
+def kakao_conv_share(txt_file):
+    with open(txt_file,"r",encoding="utf-8") as fr:
+        lines = fr.readlines() 
+        text = ""
+        for i, line in enumerate(lines[5:],1):
+            if '] [' in line:
+                text += line.split('[')[1].replace(']',' ') +str(i)+ ' '
+                print(line.split('[')[1].replace(']',' ') +str(i)+ ' ')   
+    return text
+     
 # 몸체
-txt_list = found_kakao_txt()
-wc = WordCloud(font_path='‪C:\Windows\Fonts\H2GTRE.TTF',background_color="white",width=1000,height=1000,max_words=100,max_font_size=300)
-for i, txt_file in enumerate(txt_list, 1):
-    result_txt = "result/result_"+str(i)+".txt"
-    result_png = "result/result_"+str(i)+".png"
-    kakaotalk_printer("txt/"+txt_file,result_txt)
-    kakao_to_cloud(result_txt, result_png, wc)
+if __name__ == '__main__':
+    print("안녕하세요, 분석시뮬레이터 입니다.")
+    x = ""
+    while True:
+        print("\n분석하고자 하는 분야를 선택해주세요\n만약 분석을 원하지 않는다면 exit를 입력해주세요")
+        print("1: 대화내용 분석\n2: 대화 비율 분석\n3: 카톡방 활성화 분석\n")
+        print("입력: ",end='')
+        x = input()
+        if(x=="exit"):
+            print("시뮬레이터를 종료합니다.")
+            break
+        elif(x=='1'):
+            txt_list = found_kakao_txt()
+            wc = WordCloud(font_path='‪C:\Windows\Fonts\H2GTRE.TTF',background_color="white",width=1000,height=1000,max_words=100,max_font_size=300)
+            for i, txt_file in enumerate(txt_list, 1):
+                text = ''
+                result_png = "Kakaotalk_crawling/result/conv_"+str(i)+".png"
+                text = kakao_conv_rate("Kakaotalk_crawling/txt/"+txt_file)
+                wc.generate(text)
+                wc.to_file(result_png)
+        elif(x=='2'):
+            txt_list = found_kakao_txt()
+            wc = WordCloud(font_path='‪C:\Windows\Fonts\H2GTRE.TTF',background_color="white",width=400,height=400,max_words=20,max_font_size=150)
+            for i, txt_file in enumerate(txt_list, 1):
+                text = ''
+                result_png = "Kakaotalk_crawling/result/share_"+str(i)+".png"
+                text = kakao_conv_share("Kakaotalk_crawling/txt/"+txt_file)
+                wc.generate(text)
+                wc.to_file(result_png)
+        else:
+            print("무야호~")
+    
+    
 
 
 
